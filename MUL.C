dff2cadcbd25c15bde36209da249b5be668e7a9c
thameshprabhu <30828812+thameshprabhu@users.noreply.github.com>
2017-08-08 15:52:48 +0530

    Create MUL.C

---
+++ MUL.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter the table number");
+scanf("%d",&n);
+printf("multiplication table");
+for(i=1;i<=n;i++)
+{
+m=i*n;
+printf("%d %d %d",i*n=m);
+}
+}
