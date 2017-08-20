a34dd038b7929cc6e1dbcac198cdd01df7386cf3
thameshprabhu <30828812+thameshprabhu@users.noreply.github.com>
2017-08-20 15:16:49 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter three numbers:");
+scanf("%d %d %d",&a,&b,&c);
+printf("to find largest of three numbers");
+if((a>b)&&(a>c))
+{
+printf("%d is larger",a);
+}
+else if((b>a)&&(b>c))
+{
+printf("%d is larger",b);
+}
+else
+{
+printf(%d is larger",c);
+}
+}
