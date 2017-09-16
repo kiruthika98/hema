4df3242543ffb17ead8ce31ffa8fafc768663c34
thameshprabhu <30828812+thameshprabhu@users.noreply.github.com>
2017-09-16 19:34:00 +0530

    Create characters.c

---
+++ characters.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+void main()
+{
+   char s[100];
+   int i = 0, l = 0, f = 1;
+   puts("Enter any string\n");
+   gets(str);
+   for(i = 0; s[i] !='\0'; i++)
+   {
+      l = l + 1;
+   }
+   printf("The number of characters in the string are %d", l);
+   }
