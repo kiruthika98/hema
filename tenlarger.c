6960d3c1d2694a43f933e4d78dedeb86f16cdf60
thameshprabhu <30828812+thameshprabhu@users.noreply.github.com>
2017-09-17 17:03:18 +0530

    Create tenlarger.c

---
+++ tenlarger.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+  void main() {
+    int s[10];
+    int i;
+    int greater;
+    printf("Enter ten values:");
+    for (i = 0; i < 10; i++) {
+    	scanf("%d", &a[i]);
+    }
+    greater = s[0];
+    for (i = 0; i < 10; i++)
+    {
+   	if (s[i] > greatest)
+  {
+	greater = s[i];
+    }
+    }
+    printf("Greatest of ten numbers is %d", greater);
+  }
