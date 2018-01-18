1fb482be44062983523cf98262d7ef626f1f4279
thameshprabhu <30828812+thameshprabhu@users.noreply.github.com>
2018-01-18 16:51:51 +0530

    Create highlow.c

---
+++ highlow.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main()
+{
+int a,i,min,max;
+int b[10];
+scanf("%d",&a);
+for(i=0;i<a;i++)
+{
+scanf("%d",&b[i]);
+}
+min=max=b[0];
+printf("smallest no is");
+for(i=0;i<a;i++)
+{
+if(b[i]<min)
+{
+min=b[i];
+}
+}
+printf("%d",min);
+printf("largest no is");
+for(i=0;i<a;i++)
+{
+if(b[i]>max)
+{
+max=b[i];
+}
+}
+printf("%d",max);
+}
+
