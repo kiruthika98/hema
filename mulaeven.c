3cb06f14c52b5af55ea23891f61b3ab9c213d790
thameshprabhu <30828812+thameshprabhu@users.noreply.github.com>
2018-01-28 16:13:23 +0530

    Create mulaeven.c

---
+++ mulaeven.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a;
+scanf("%d",&a);
+if(a%7==0)
+{
+printf("given no is multiple of 7");
+}
+else
+{
+printf("given no iis not a multiple of 7");
+}
+return 0;
+}
