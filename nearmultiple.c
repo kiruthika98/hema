dab4b44337aecff85a1a5e75e53d8d0a1c856555
thameshprabhu <30828812+thameshprabhu@users.noreply.github.com>
2018-01-28 16:06:04 +0530

    Create nearmultiple.c

---
+++ nearmultiple.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int num,n;
+    printf("enter the number");
+    scanf("%d",&num);
+    while(number%10!=0)
+    {
+        numb++;
+    }
+printf("\nnearest greater multiple of 10 %d",num);
+    }
