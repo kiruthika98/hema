363c1fa0526f4f0f62096c9a10ed3a562b49b193
thameshprabhu <30828812+thameshprabhu@users.noreply.github.com>
2018-01-28 15:57:56 +0530

    Create binary.c

---
+++ binary.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n,temp,count=0,k;
+	printf("\nEnter the number: ");
+	scanf("%d",&n);
+	k=n;
+	while(n!=0)
+	{
+		temp=n%10;
+		count=0;
+		if(temp==1||temp==0)
+		{
+			count=1;
+		}
+		else
+		{
+			break;
+		}
+		n=n/10;
+	}
+	if(count==1)
+	{
+		printf("\nThe number %d is binary",k);
+	}
+	else
+	{
+		printf("\nThe number %d is not binary",k);
+	}
+	return 0;
+}
