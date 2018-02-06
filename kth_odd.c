9cae49b1b0c4ab7fe17d18eef8429951d6254ecc
thameshprabhu <30828812+thameshprabhu@users.noreply.github.com>
2018-02-06 10:32:15 +0000

    Create kth_odd.c

---
+++ kth_odd.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+int a[10],i,n,t,j,k;
+printf("enter the range\n");
+scanf("%d",&n);
+scanf("%d",&k);
+printf("enter the array values\n");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+    if(a[i]%2!=0)
+    {
+        t++;
+    }
+    if(t==k)
+    {
+        printf("%d",a[i]);
+        break;
+    }
+}
+}
+
