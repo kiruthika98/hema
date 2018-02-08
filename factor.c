8ac1e21994854b6aa8d903ed947e5ec9db3053f2
thameshprabhu <30828812+thameshprabhu@users.noreply.github.com>
2018-02-08 10:57:25 +0000

    Create factor.c

---
+++ factor.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int n,i,flag=0;
+  printf("enter the number");
+  scanf("%d",&n);
+  printf("the factors are");
+  for(i=1;i<=n;i++)
+  {
+  if(n%i==0)
+  {
+    printf("%d\t",i);
+  }
+  }
+  return 0;
+}
