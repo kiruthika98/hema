83d9fd53a843e53e1d36b23ea7e26ac0cc909c9b
thameshprabhu <30828812+thameshprabhu@users.noreply.github.com>
2018-02-08 10:53:07 +0000

    Create *.c

---
+++ *.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char str[100];
+gets(str);
+int len,i;
+len=strlen(str);
+if(len%2!=0)
+{str[(len-1)/2]='*';
+}else
+{
+str[len/2]='*';
+str[(len/2)-1]='*';
+}
+printf("%s",str);
+}
