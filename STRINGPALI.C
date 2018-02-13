8c88713ac334b5ed364b97543afe293427a42cbc
thameshprabhu <30828812+thameshprabhu@users.noreply.github.com>
2018-02-13 16:04:56 +0530

    Create STRINGPALI.C

---
+++ STRINGPALI.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[10],b[10];
+int c;
+printf("enter the string");
+scanf("%s",&a);
+strcpy(b,a);
+strrev(a);
+c=strcmp(a,b);
+if(b==0)
+{
+printf("palindrome");
+}
+else
+{
+    printf("not a palindrome");
+}
+return 0;
+}
